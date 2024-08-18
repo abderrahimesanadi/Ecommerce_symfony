dd99c3fc7c9f6747538a0b81299b6d07bc3771a3
Abderrahime Sanadi <abderrahime.sanadi@gmail.com>
2024-08-18 20:11:34 +0000

    Update AdminCategoryController.php

--- src/Controller/Admin/AdminCategoryController.php
+++ src/Controller/Admin/AdminCategoryController.php
@@ -16,7 +16,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 /**
  * Require ROLE_ADMIN for *every* controller method in this class.
  *
- * @IsGranted("ROLE_ADMIN")
+ * @IsGranted("ROLE_USER")
  */
 
 class AdminCategoryController extends AbstractController
